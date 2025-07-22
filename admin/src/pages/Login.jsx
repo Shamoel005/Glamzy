573c5288197784febbb07edca149031ca8577b14
Md Shamoel Farooqui <farooquishamoel@gmail.com>
2025-07-22 15:36:36 +0530

    Update Login.jsx

--- admin/src/pages/Login.jsx
+++ admin/src/pages/Login.jsx
@@ -37,8 +37,8 @@ function Login() {
   return (
     <div className='w-[100vw] h-[100vh] bg-gradient-to-l from-[#141414] to-[#0c2025] text-[white] flex flex-col items-center justify-start'>
        <div className='w-[100%] h-[80px] flex items-center justify-start px-[30px] gap-[10px] cursor-pointer' >
-       <img className='w-[40px]' src={logo} alt="" />
-       <h1 className='text-[22px] font-sans '>OneCart</h1>
+       <img className='w-[120px]' src={logo} alt="" />
+       <h1 className='text-[22px] font-sans '></h1>
        </div>
    
        <div className='w-[100%] h-[100px] flex items-center justify-center flex-col gap-[10px]'>
