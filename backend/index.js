043ffa2aad60998c99a075d19bf5746ae2470fdf
Md Shamoel Farooqui <farooquishamoel@gmail.com>
2025-07-22 15:32:46 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ let app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
- origin:["https://glamzy-frontend.onrender.com" , "http://localhost:5174"],
+ origin:["https://glamzy-frontend.onrender.com" , "https://glamzy-admin.onrender.com"],
  credentials:true
 }))
 
